dfcd9cd318c228188c4e8937d657f6f42de4961a
actions-user <action@github.com>
2023-10-06 05:41:24 +0000

    build: 4f5eb699b01b438652d76c96dbf06b7b408c7cbb

--- dist/index.js
+++ dist/index.js
@@ -2,15 +2,26 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.setMockConsole = exports.YandexCloudSimpleLogger = void 0;
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const format = require("quick-format-unescaped"); // same formater as the one is used in pino.js
-const DEFAULT_ENV_KEY = "LOG_LEVEL";
-const DEFAULT_LEVEL = "info";
-const silentLogFn = () => {
-};
+const format = require('quick-format-unescaped'); // same formater as the one is used in pino.js
+const DEFAULT_ENV_KEY = 'LOG_LEVEL';
+const DEFAULT_LEVEL = 'info';
+const silentLogFn = () => { };
 const yandexCloudLogFnBuilder = (level) => {
     const LEVEL = level.toUpperCase();
     return function log(objOrMsg, ...args) {
-        if (typeof objOrMsg === "object") {
+        const prefix = [];
+        // if (this.showTimestamp) {
+        //     prefix.push(new Date().toISOString());
+        // }
+        //
+        // if (this.showLevel) {
+        //     prefix.push(LEVEL);
+        // }
+        if (this.prefix) {
+            prefix.push(this.prefix);
+        }
+        const prefixStr = prefix.length === 0 ? '' : `[${prefix.join(' ')}] `;
+        if (typeof objOrMsg === 'object') {
             // TODO: Later add from pino
             // if (msg === null && n.length === 0) {
             //     formatParams = [null]
@@ -18,16 +29,31 @@ const yandexCloudLogFnBuilder = (level) => {
             //     msg = n.shift()
             //     formatParams = n
             // }
-            consoleOrMock[level === "error" ? "error" : "log"](JSON.stringify({
-                ...(objOrMsg instanceof Error ? { stack: objOrMsg.stack } : objOrMsg),
-                level: LEVEL,
-                msg: format(args[0] ?? level, args.splice(1))
-            }));
+            if (typeof args[0] === 'string') {
+                consoleOrMock.log(JSON.stringify({
+                    ...(objOrMsg instanceof Error
+                        ? { stack: objOrMsg.stack }
+                        : objOrMsg),
+                    level: LEVEL,
+                    msg: format(`${prefixStr}${args[0]}`, args.splice(1)),
+                }));
+            }
+            else {
+                consoleOrMock.log(JSON.stringify({
+                    ...(objOrMsg instanceof Error
+                        ? { stack: objOrMsg.stack }
+                        : objOrMsg),
+                    level: LEVEL,
+                    msg: format(`${prefixStr}%o`, objOrMsg instanceof Error
+                        ? objOrMsg.message
+                        : objOrMsg),
+                }));
+            }
         }
         else {
-            consoleOrMock[level === "error" ? "error" : "log"](JSON.stringify({
+            consoleOrMock.log(JSON.stringify({
                 level: LEVEL,
-                msg: format(objOrMsg, args)
+                msg: format(`${prefixStr}${objOrMsg}`, args),
             }));
         }
     };
@@ -37,6 +63,7 @@ const yandexCloudLogFnBuilder = (level) => {
  */
 class YandexCloudSimpleLogger {
     constructor(options = {}) {
+        this.fatal = silentLogFn;
         this.error = silentLogFn;
         this.warn = silentLogFn;
         this.info = silentLogFn;
@@ -44,9 +71,13 @@ class YandexCloudSimpleLogger {
         this.trace = silentLogFn;
         let { level, 
         // eslint-disable-next-line prefer-const
+        prefix, 
+        // eslint-disable-next-line prefer-const
         showTimestamp, 
         // eslint-disable-next-line prefer-const
-        showLevel } = options;
+        showLevel, } = options;
+        if (prefix)
+            this.prefix = prefix;
         this.showTimestamp = showTimestamp ?? false;
         this.showLevel = showLevel ?? false;
         // eslint-disable-next-line no-param-reassign
